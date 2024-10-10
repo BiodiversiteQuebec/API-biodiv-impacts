1b1724e86cac9d56a53b5417a71d064fcf427a6e
Alexisb <alexisbrou1@hotmail.fr>
2024-10-10 14:11:40 -0400

    woohoo

--- app/indicators/indicateur1.py
+++ app/indicators/indicateur1.py
@@ -1,2 +1,26 @@
+import time
+from functools import wraps
+
+
+def timing_decorator(func):
+    """
+    Measures the execution time of a function.
+
+    Args:
+        func (function): Function to be timed.
+
+    Returns:
+        tuple: Function result and elapsed time.
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        return result, elapsed_time
+    return wrapper
+
+@timing_decorator
 def compute_indicateur1():
     return 42
--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import enum
 from typing import List, Tuple
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -14,10 +15,17 @@ async def root():
     return {"message": "Hello World"}
 
 
+def generate_response(indicators: List[Tuple[str, float]]) -> dict:
+    return {f"indicator_{i+1}": {"value": value, "elapsed_time": elapsed_time} for i, (value, elapsed_time) in enumerate(indicators)}
+
 # This is where you could put your code for Indicateur 1
 @app.post("/indicateurs")
 async def indicateurs(request: IndicateursRequest):
     poly, eee_qstr, pollution_qstr = request.poly, request.eee_qstr, request.pollution_qstr
 
-    ind1 = compute_indicateur1(poly, eee_qstr, pollution_qstr)
-    return {"message": "Hello World"}
+    indicators = []
+    indicators.append(compute_indicateur1())
+
+
+    res = generate_response(indicators)
+    return res
