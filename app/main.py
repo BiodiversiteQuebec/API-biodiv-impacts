9f6dfbf35bcc786e08c2da9ca846b3f6077c22cf
vcameron1 <victor.cameron@usherbrooke.ca>
2025-04-04 15:58:51 -0400

    set return as full json output

--- app/main.py
+++ app/main.py
@@ -63,15 +63,14 @@ def connectivity_indicator(geojson: dict):
 
     Args:
 
-        geojson (dict): A dictionary representing the GeoJSON object containing
-                        geographical features.
+        geojson (dict): A dictionary representing the GeoJSON object containing geographical features.
 
     Returns:
 
-        dict: A dictionary containing the computed connectivity indicator with the key "Connectivité".
+        dict: A dictionary containing the computed connectivity indicator with intermediate results.
     """
     result = compute_connectivity(geojson)
-    return {"Connectivité": result}
+    return result
 
 @app.post("/carbon_stocks")
 def carbon_stocks_indicator(geojson: dict):
@@ -88,11 +87,10 @@ def carbon_stocks_indicator(geojson: dict):
 
     Returns:
 
-        dict: A dictionary with the key "Stocks de carbone" and the computed
-            carbon stocks as the value.
+        dict: A dictionary with the carbons stock indicator and intermediate results.
     """
     result = compute_carbon_stocks(geojson)
-    return {"Stocks de carbone": result}
+    return result
 
 @app.post("/pollution")
 def pollution_indicator(survey: dict):
@@ -105,10 +103,10 @@ def pollution_indicator(survey: dict):
 
     Returns:
 
-        dict: A dictionary with the computed pollution indicator.
+        dict: A dictionary with the computed pollution indicator and intermediate results.
     """
     result = compute_pollution(survey)
-    return {"Pollution": result}
+    return result
 
 @app.post("/conservation")
 def conservation_indicator(geojson: dict, influence_distance: float):
@@ -126,7 +124,7 @@ def conservation_indicator(geojson: dict, influence_distance: float):
         dict: A dictionary containing the conservation indicator result.
     """
     result = compute_conservation(geojson, influence_distance)
-    return {"Conservation": result}
+    return result
 
 @app.post("/ecosystem_services")
 def ecosystem_services_indicator(geojson: dict, distance_max: float, milieu: str = "urbain"):
@@ -146,10 +144,10 @@ def ecosystem_services_indicator(geojson: dict, distance_max: float, milieu: str
         dict: A dictionary containing the calculated ecosystem services.
     """
     result = compute_ecosystem_services(geojson, distance_max, milieu)
-    return {"Services écosystémiques": result}
+    return result
 
 @app.post("/habitat_destruction")
-def habitat_destruction_indicator(geojson: dict):
+def habitat_destruction_indicator(geojson: dict = { "type": "Polygon", "coordinates": [ [ [ -71.923109522861893, 45.37836376463364 ], [ -71.923149350064904, 45.373355928601626 ], [ -71.912435832458442, 45.373271994610192 ], [ -71.912515486864436, 45.378307813608295 ], [ -71.923109522861893, 45.37836376463364 ] ] ] }):
     """
     Endpoint to calculate the habitat destruction indicator.
 
@@ -162,10 +160,10 @@ def habitat_destruction_indicator(geojson: dict):
         dict: A dictionary containing the calculated habitat destruction indicator.
     """
     result = compute_habitat_destruction(geojson)
-    return {"Destruction de l'habitat": result}
+    return result
 
 @app.post("/human_footprint")
-def human_footprint_indicator(geojson: dict):
+def human_footprint_indicator(geojson: dict = { "type": "Polygon", "coordinates": [ [ [ -71.923109522861893, 45.37836376463364 ], [ -71.923149350064904, 45.373355928601626 ], [ -71.912435832458442, 45.373271994610192 ], [ -71.912515486864436, 45.378307813608295 ], [ -71.923109522861893, 45.37836376463364 ] ] ] }):
     """
     Endpoint to calculate the human footprint indicator.
 
@@ -178,12 +176,12 @@ def human_footprint_indicator(geojson: dict):
         dict: A dictionary containing the calculated human footprint indicator.
     """
     result = compute_human_footprint(geojson)
-    return {"Empreinte humaine": result}
+    return result
 
 @app.post("/invasive_exotics")
 def invasive_exotics_indicator(
-        geojson: dict = Body(...),
-        survey: dict = Body(...)):
+        geojson: dict = { "type": "Polygon", "coordinates": [ [ [ -71.923109522861893, 45.37836376463364 ], [ -71.923149350064904, 45.373355928601626 ], [ -71.912435832458442, 45.373271994610192 ], [ -71.912515486864436, 45.378307813608295 ], [ -71.923109522861893, 45.37836376463364 ] ] ] },
+        survey: dict = {"Q1": False, "Q2": False, "Q3": False, "Q4": False, "Q5": False, "Q6": False, "Q7": False, "Q8": True, "Q9": True}):
     """
     Endpoint to calculate the invasive exotics indicator.
 
@@ -196,38 +194,12 @@ def invasive_exotics_indicator(
     Returns:
 
         dict: A dictionary containing the calculated invasive exotics indicator.
-
-    Usage:
-
-    {
-  "geojson": {
-    "type": "Polygon",
-    "coordinates": [
-        [
-            [-71.989021573308534, 45.404000415469056],
-            [-71.987038174572334, 45.402959401362089],
-            [-71.98540963380141, 45.404505556821491],
-            [-71.987028443356962, 45.40533493226846],
-            [-71.987140253500854, 45.405362234064313],
-            [-71.987237484510757, 45.405372474057657],
-            [-71.987315263440109, 45.405372473936815],
-            [-71.987402772819166, 45.405369066780658],
-            [-71.987480546508735, 45.405351995013334],
-            [-71.987543748498041, 45.405324697182159],
-            [-71.987621531580913, 45.405304216052308],
-            [-71.987670135059872, 45.405266675018261],
-            [-71.989021573308534, 45.404000415469056]
-        ]
-    ]
-  },
-  "survey": {"Q1": false, "Q2": false, "Q3": false, "Q4": false, "Q5": false, "Q6": false, "Q7": false, "Q8": false, "Q9": true, "Q10": true}
-}
     """
     result = compute_invasive_exotics(geojson, survey)
-    return {"Espèces exotiques envahissantes": result}
+    return result
 
 @app.post("/species_at_risk")
-def species_at_risk_indicator(geojson: dict):
+def species_at_risk_indicator(geojson: dict = { "type": "Polygon", "coordinates": [ [ [ -71.923109522861893, 45.37836376463364 ], [ -71.923149350064904, 45.373355928601626 ], [ -71.912435832458442, 45.373271994610192 ], [ -71.912515486864436, 45.378307813608295 ], [ -71.923109522861893, 45.37836376463364 ] ] ] }):
     """
     Endpoint to calculate the species at risk indicator.
 
@@ -240,4 +212,4 @@ def species_at_risk_indicator(geojson: dict):
         dict: A dictionary containing the calculated species at risk indicator.
     """
     result = compute_species_at_risk(geojson)
-    return {"Espèces en péril": result}
+    return result
